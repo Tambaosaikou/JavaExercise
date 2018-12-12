7058bf4ee64ee83a8f2617ee029618cb70cf2338
汪隽扬 <Tambaosaikou@gmail.com>
2018-12-12 19:46:17 +0800

    去除字符串中所有的空格。例如 a b c d e  f --> abcdef

--- Day1212/src/test1/Test1.java
+++ Day1212/src/test1/Test1.java
@@ -1,9 +1,16 @@
+/**
+ * 去除字符串中所有的空格。例如 a b c d e  f --> abcdef
+ */
+
 package test1;
 
 public class Test1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		String str = "a b c d e  f";
+		str = str.replace(" ", "");
+		System.out.println(str);
 
 	}
 
